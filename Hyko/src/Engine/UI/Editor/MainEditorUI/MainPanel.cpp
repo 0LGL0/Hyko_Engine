2f20554b3d408dab06a3a9beb4dd66a1eb5f3df4
LGL <artemkohan36@gmail.com>
2023-02-28 22:51:44 +0300

    Добавлен основной Menu Bar

--- Hyko/src/Engine/UI/Editor/MainEditorUI/MainPanel.cpp
+++ Hyko/src/Engine/UI/Editor/MainEditorUI/MainPanel.cpp
@@ -23,6 +23,28 @@ void Hyko::EMainPanel::init()
 		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
 		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockFlags);
 
+		if (ImGui::BeginMenuBar()){
+
+			if (ImGui::BeginMenu("File")) {
+				
+				ImGui::EndMenu();
+			}
+
+			if (ImGui::BeginMenu("Edit")) {
+
+				ImGui::EndMenu();
+			}
+
+			if (ImGui::BeginMenu("Help")) {
+				if (ImGui::IsItemHovered())
+					ImGui::SetTooltip("Coming soon");
+
+				ImGui::EndMenu();
+			}
+
+			ImGui::EndMenuBar();
+		}
+
 		m_viewport.init();
 		m_hierarchy.init();
 		m_componentsSettings.init(m_hierarchy.m_selectedEntity);
